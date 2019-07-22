0a7e62529eb736477103fadf5582e0fa73dd0a95
EricNwagwu <Eric.nwagwu@mavs.uta.edu>
2019-07-22 02:39:39 -0500

    Updates

---
+++ precache-manifest.8f5b51bebb6cb8c5c81a5fe985706c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8585a5bc26ddf215f22e17d45e8cfcc",
+    "url": "/QuoteMachine/index.html"
+  },
+  {
+    "revision": "00feaedb672174ff1f50",
+    "url": "/QuoteMachine/static/css/2.5d45f07d.chunk.css"
+  },
+  {
+    "revision": "64fa91af503671ee01f4",
+    "url": "/QuoteMachine/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "00feaedb672174ff1f50",
+    "url": "/QuoteMachine/static/js/2.85a11bac.chunk.js"
+  },
+  {
+    "revision": "64fa91af503671ee01f4",
+    "url": "/QuoteMachine/static/js/main.105f77cc.chunk.js"
+  },
+  {
+    "revision": "ab1f7b065459d22f075b",
+    "url": "/QuoteMachine/static/js/runtime~main.573c12d3.js"
+  }
+]);
